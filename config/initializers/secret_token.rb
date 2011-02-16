07c6d1b1684760f63febf596e2c9c8eac060714d
Marco Yin <euyniy@gmail.com>
2011-02-16 21:21:52 +0800

    initial repo

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ongile::Application.config.secret_token = 'a9486d4dc022365d506f54b010da72dc891cc07b8858c403db3119f4f0a7440cddfb4669ef4eca59fa8210f7cd5f90933957f24ca1914da876a9f874790ae134'
